785db3f555bd9fa16eb3825c28f08214ea5f0c83
tapaswishende <97527212+tapaswishende@users.noreply.github.com>
2024-10-09 21:23:34 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Next.js App</h1>
+    </div>
+  );
+};
+
+export default Home;
+ 
